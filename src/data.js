cf663ce13eae67b3be6fb089d6192adbe9e90045
Kasim Nadim <37031076+kasimnadim6@users.noreply.github.com>
2022-09-07 21:04:23 +0530

    data file

---
+++ src/data.js
@@ -0,0 +1,83 @@
+export const Books = [
+  {
+    id: 1,
+    author_id: 4,
+    title: 'Norwegian Wood',
+    cover_image:
+      'http://t1.gstatic.com/images?q=tbn:ANd9GcQvJJDi2mzwg9v_PlmKYL31gXIz0kvAObJak7DVFPcD_mJTIyec',
+    pages: 296,
+    releaseDate: '1987',
+    isbn: 'hj846',
+  },
+  {
+    id: 2,
+    author_id: 4,
+    title: 'Kafka on the Shore',
+    cover_image:
+      'http://t0.gstatic.com/images?q=tbn:ANd9GcRHFU_j93PPsbQGqoaZJnHH6-Emk_sIxG823SkoRTL0nvdEP41f',
+    pages: 505,
+    releaseDate: '2002',
+    isbn: 'op012',
+  },
+  {
+    id: 3,
+    author_id: 4,
+    title: 'After Dark',
+    cover_image:
+      'http://t3.gstatic.com/images?q=tbn:ANd9GcQBMNA8A19vQpNY4bkgadsLhiRUFqBKwKAA6ANrw8VEtOiPrYQJ',
+    pages: 208,
+    releaseDate: '2004',
+    isbn: 'cv456',
+  },
+  {
+    id: 4,
+    author_id: 4,
+    title: '1Q84',
+    cover_image:
+      'http://t0.gstatic.com/images?q=tbn:ANd9GcTBQZSg-b2LFkLlt9fWndS1w8SONabDZBHf0dtdb3-bqcuKxduL',
+    pages: 928,
+    releaseDate: '2009',
+    isbn: 'al207',
+  },
+  {
+    id: 5,
+    author_id: 4,
+    title: 'Norwegian Wood',
+    cover_image:
+      'http://t1.gstatic.com/images?q=tbn:ANd9GcQvJJDi2mzwg9v_PlmKYL31gXIz0kvAObJak7DVFPcD_mJTIyec',
+    pages: 296,
+    releaseDate: '1987',
+    isbn: 'hj846',
+  },
+
+  {
+    id: 6,
+    author_id: 4,
+    title: 'Norwegian Wood',
+    cover_image:
+      'http://t1.gstatic.com/images?q=tbn:ANd9GcQvJJDi2mzwg9v_PlmKYL31gXIz0kvAObJak7DVFPcD_mJTIyec',
+    pages: 296,
+    releaseDate: '1987',
+    isbn: 'hj846',
+  },
+  {
+    id: 7,
+    author_id: 4,
+    title: 'Norwegian Wood',
+    cover_image:
+      'http://t0.gstatic.com/images?q=tbn:ANd9GcTBQZSg-b2LFkLlt9fWndS1w8SONabDZBHf0dtdb3-bqcuKxduL',
+    pages: 928,
+    releaseDate: '2009',
+    isbn: 'al207',
+  },
+  {
+    id: 8,
+    author_id: 4,
+    title: 'Norwegian Wood',
+    cover_image:
+      'http://t1.gstatic.com/images?q=tbn:ANd9GcQvJJDi2mzwg9v_PlmKYL31gXIz0kvAObJak7DVFPcD_mJTIyec',
+    pages: 296,
+    releaseDate: '1987',
+    isbn: 'hj846',
+  },
+];
